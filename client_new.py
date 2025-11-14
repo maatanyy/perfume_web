ae9a7c428441d6306d54cd457dc1a5f1cef9c164
maatanyy <maatanyy@naver.com>
2025-11-15 00:34:32 +0900

    fix port

--- client_new.py
+++ client_new.py
@@ -258,5 +258,5 @@ if __name__ == '__main__':
     
     threading.Thread(target=open_browser, daemon=True).start()
     
-    app.run(debug=False, host='127.0.0.1', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
 
